f3ce9ef3534e91f6b53f5f26e7fb0216062e18c4
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2026-04-22 14:43:55 +0100

    docs: add Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Insighta Labs — Profiles API')
+    .setDescription('API documentation for Insighta Labs — Profiles API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
 
   app.enableCors({
     origin: '*',
@@ -19,6 +28,8 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
